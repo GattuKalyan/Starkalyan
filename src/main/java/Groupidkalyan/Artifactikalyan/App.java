73a6acddaaed432e4cf3848bc6501c255aeb3a56
Kalyan <Kalyan@DESKTOP-HIGVP14>
2022-06-16 19:32:11 +0530

    kalyan: june 16 2022 :Adding project first time to github

---
+++ src/main/java/Groupidkalyan/Artifactikalyan/App.java
@@ -0,0 +1,13 @@
+package Groupidkalyan.Artifactikalyan;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

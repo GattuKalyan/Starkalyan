7d5c252deee632996eb009d23395e621902d2e3f
Amruth <amruthnani28@gmail.com>
2022-06-23 17:28:16 +0530

    zzzz

--- src/main/java/Groupidkalyan/Artifactikalyan/App.java
+++ src/main/java/Groupidkalyan/Artifactikalyan/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
     int Amruth=007;
+    int kalyan= 143;
      system.out.println("Amruth");
         System.out.println( "Hello World!" );
     }

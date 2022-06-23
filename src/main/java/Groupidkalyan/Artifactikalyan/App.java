289c573a1f6a70c41f0aaaed8835cb83519289a7
AmruthDoggela <107615680+AmruthDoggela@users.noreply.github.com>
2022-06-23 17:28:36 +0530

    Update App.java

--- src/main/java/Groupidkalyan/Artifactikalyan/App.java
+++ src/main/java/Groupidkalyan/Artifactikalyan/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
     int Amruth=007;
-     
+     system.out.println("Amruth");
         System.out.println( "Hello World!" );
     }
 }
